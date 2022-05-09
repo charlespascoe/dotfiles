713a68babe26f22f83d873e02aef27ae2db287fe
Charles Pascoe <charles@cpascoe.co.uk>
2022-05-09 18:49:54 +0100

    serenade/custom.js: remove unnecessary comment, add some JavaScript commands

--- serenade/custom.js
+++ serenade/custom.js
@@ -1,27 +1,4 @@
-/* Serenade Custom Commands
-
-In this file, you can define your own custom commands with the Serenade API.
-
-For instance, here's a custom automation that opens your terminal and runs a command:
-
-serenade.global().command("make", api => {
-  api.focusApplication("terminal");
-  api.typeText("make clean && make");
-  api.pressKey("return");
-});
-
-And, here's a Python snippet for creating a test method:
-
-serenade.language("python").snippet(
-  "test method <%identifier%>",
-  "def test_<%identifier%>(self):<%newline%><%indent%>pass",
-  { "identifier": ["underscores"] }
-  "method"
-);
-
-For more information, check out the Serenade API documentation: https://serenade.ai/docs/api
-
-*/
+// Vim Commands //
 
 const vim = serenade.app('Vim');
 
@@ -60,4 +37,28 @@ vim.command(
   'close file',
   (api) => api.evaluateInPlugin('Bdelete'),
   {autoExecute: false},
+);
+
+// JavaScript Commands //
+
+const javascript = serenade.language('javascript');
+
+javascript.command('preview', async (api) => {
+  await api.runCommand('save');
+  await api.focusApplication('chrome');
+  await api.runCommand('reload');
+});
+
+javascript.snippet(
+  'add element <%identifier%>',
+  '<%identifier%>()(<%cursor%>)',
+  { 'identifier': ['camel'] },
+  'inline',
+);
+
+javascript.snippet(
+  'object',
+  '{<%cursor%>}',
+  {},
+  'inline',
 );
