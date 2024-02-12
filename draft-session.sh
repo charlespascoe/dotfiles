4c7e045f6644e5e7d405de4b49de8f127c7fa5a0
Charles Pascoe <charles@cpascoe.co.uk>
2024-02-12 11:42:11 +0000

    Various tweaks and improvements

--- draft-session.sh
+++ draft-session.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 tmux new-session -d -s draft \
-    \; set-buffer "vim -c DraftReturn" \
+    \; set-buffer "sleep 2; while true; do vim -c DraftReturn; sleep 5; done" \
     \; paste-buffer -t draft: \
     \; send-keys -t draft: C-m
