2a4f344f34d17ba3433811c1674ad79e35b8865f
Charles Pascoe <charles@cpascoe.co.uk>
2025-12-31 15:09:23 +0000

    draft-session.sh: Use DraftReturnMarkdown instead of DraftReturn (which uses Bulletnotes)

--- draft-session.sh
+++ draft-session.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 tmux new-session -d -s draft \
-    \; set-buffer "sleep 2; while true; do vim -c DraftReturn; sleep 5; done" \
+    \; set-buffer "sleep 2; while true; do vim -c DraftReturnMarkdown; sleep 5; done" \
     \; paste-buffer -t draft: \
     \; send-keys -t draft: C-m
