01102d4dbef73de77d223fde2fd9ac589e54b80d
Charles Pascoe <charles@cpascoe.co.uk>
2026-04-09 15:53:22 +0100

    draft-session.sh: Add DRAFT env to allow customising Vim plugins in draft mode

--- draft-session.sh
+++ draft-session.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 tmux new-session -d -s draft \
-    \; set-buffer "sleep 2; while true; do vim -c DraftReturnMarkdown; sleep 5; done" \
+    \; set-buffer "export DRAFT=1; sleep 2; while true; do vim -c DraftReturnMarkdown; sleep 5; done" \
     \; paste-buffer -t draft: \
     \; send-keys -t draft: C-m
